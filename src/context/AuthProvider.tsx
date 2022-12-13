ec86fa3e2a6990ee50945206833be6914aa71716
searchme86 <searchme86@gmail.com>
2022-12-13 16:27:37 +0900

    🐛 fix context type: does not exist on type{}

--- src/context/AuthProvider.tsx
+++ src/context/AuthProvider.tsx
@@ -1,27 +1,36 @@
-import { createContext, ReactElement, useState } from 'react';
+import { createContext, ReactElement, SetStateAction, useState } from 'react';
 
 interface AuthProviderInterface {
   children: ReactElement;
 }
 
-interface AuthProps {
-  auth: {
-    user: string;
-    pwd: string;
-    roles: number[];
-    accessToken: string;
-  };
+interface AuthTokenShape {
+  pwd: string;
+  roles: number[];
+  accessToken: string;
+  user: string;
 }
 
-const AuthContext = createContext({} as AuthProps);
-
-interface Something {
-  auth: AuthProps['auth'];
-  setAuth: (auth: AuthProps) => void;
+interface ContextAuthType {
+  auth: AuthTokenShape;
+  setAuth: React.Dispatch<SetStateAction<AuthTokenShape>>;
 }
+
+const ContextInitialValue: AuthTokenShape = {
+  pwd: '',
+  roles: [],
+  accessToken: '',
+  user: '',
+};
+
+const AuthContext = createContext<ContextAuthType>({
+  auth: ContextInitialValue,
+  setAuth: () => null,
+});
 
 export const AuthProvider = ({ children }: AuthProviderInterface) => {
-  const [auth, setAuth] = useState<Something>({} as Something);
+  const [auth, setAuth] =
+    useState<ContextAuthType['auth']>(ContextInitialValue);
 
   return (
     <AuthContext.Provider value={{ auth, setAuth }}>
--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -4,6 +4,7 @@ import AuthContext from '../context/AuthProvider';
 const useAuth = () => {
   const { auth } = useContext(AuthContext);
   useDebugValue(auth, (auth) => (auth?.user ? 'Logged In' : 'Logged Out'));
+
   return useContext(AuthContext);
 };
 
